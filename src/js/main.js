b0686050a1325a9b4bf06253ff741c50f7ca24a1
Onotype <16868065+onotype@users.noreply.github.com>
2023-10-20 06:17:00 +0800

    Bulk generator development

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ let downloadButtons = document.querySelectorAll('.generate-button');
 
 window.onload = function () {
   generator(partNumber.value);
+  lister();
 };
 
 partNumber.addEventListener('input', function() {
@@ -33,7 +34,6 @@ downloadButtons.forEach(function(b) {
 function generator(val) {
   let url = document.querySelector('input[name="website"]:checked').value;
   let finalURL = url + '/' + val;
-  console.log(finalURL);
 
   let dataMatrix = DATAMatrix({
     msg: finalURL,
@@ -75,4 +75,78 @@ function download(button, name, type){
    document.body.appendChild(a);
    a.click();
    document.body.removeChild(a);
+}
+
+// Bulk Generator
+let listInput = document.querySelector('.bulk-generator input');
+
+listInput.addEventListener('input', function() {
+  lister();
+});
+
+function lister() {
+  let inputVal = listInput.value;
+  let inputArr = inputVal.split(/,\s*/);
+  let generatorTable = document.querySelector('.generator-table');
+
+  if (inputVal){
+    generatorTable.innerHTML = '';
+
+    inputArr.forEach(value => {
+      let item = document.createElement('div');
+      
+      // Make the item div
+      item.classList.add('generator-item');
+
+      // Identify the Brand
+      let itemBrand = identifyBrand(value);
+      item.classList.add('brand--' + itemBrand.toLowerCase().replace(' ', '-'));
+
+      // Identify the URL
+      let itemURL;
+
+      if(itemBrand == 'Brenthaven') {
+        itemURL = 'https://brenthaven.com/' + value;
+      } else if (itemBrand == 'Gumdrop Cases') {
+        itemURL = 'https://www.gumdropcases.com/' + value;
+      } else {
+        itemURL = 'Invalid URL';
+      }
+
+      // Insert the item content
+      item.innerHTML = `
+        <div class="generator-item__value">${value}</div>
+        <div class="generator-item__brand"><span>${itemBrand}</span></div>
+        <div class="generator-item__url">${itemURL}</div>
+        <div class="generator-item__code"></div>
+      `;
+
+      console.log(itemURL);
+      if (itemURL != 'Invalid URL'){
+        let dataMatrix = DATAMatrix({
+          msg: itemURL,
+          dim: 2000,
+          pad: 1
+        });
+        item.querySelector('.generator-item__code').replaceChildren();
+        item.querySelector('.generator-item__code').appendChild(dataMatrix);
+      } else {
+        item.querySelector('.generator-item__code').innerHTML += '<div class="empty-code"></div>';
+      }
+
+      generatorTable.appendChild(item);
+    });
+  }
+}
+
+function identifyBrand(sku) {
+  let brand;
+  if (/^\d{4}(?:$|\d{3}$)/.test(sku)) {
+    brand = 'Brenthaven';
+  } else if (/\b\d{2}[A-Za-z]\d{3}(?:$|[EC]\d{2}[-]\d(?:$|\d$))/.test(sku)) {
+    brand = 'Gumdrop Cases';
+  } else {
+    brand = 'Invalid SKU'
+  }
+  return brand;
 }
